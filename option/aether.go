fb38d2d81fdfffb49eac2a323bd64bd8d3a763f0
LI JIANTING <lijt@LIdeMacBook-Air.local>
2026-07-26 12:46:23 +0800

    feat: add application-level Ping/Pong keepalive & multiplexing support for Aether protocol

--- option/aether.go
+++ option/aether.go
@@ -2,13 +2,17 @@ package option
 
 type AetherInboundOptions struct {
 	ListenOptions
-	Password    string `json:"password"`
-	TimeLimitMs int64  `json:"time_limit_ms,omitempty"`
+	Password             string                  `json:"password"`
+	TimeLimitMs          int64                   `json:"time_limit_ms,omitempty"`
+	HeartbeatIntervalSec int                     `json:"heartbeat_interval_sec,omitempty"`
+	Multiplex            *InboundMultiplexOptions `json:"multiplex,omitempty"`
 }
 
 type AetherOutboundOptions struct {
 	DialerOptions
 	ServerOptions
-	Network  NetworkList `json:"network,omitempty"`
-	Password string      `json:"password"`
+	Network              NetworkList              `json:"network,omitempty"`
+	Password             string                   `json:"password"`
+	HeartbeatIntervalSec int                      `json:"heartbeat_interval_sec,omitempty"`
+	Multiplex            *OutboundMultiplexOptions `json:"multiplex,omitempty"`
 }
--- protocol/aether/inbound.go
+++ protocol/aether/inbound.go
@@ -7,10 +7,12 @@ import (
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/adapter/inbound"
 	"github.com/sagernet/sing-box/common/listener"
+	"github.com/sagernet/sing-box/common/mux"
 	"github.com/sagernet/sing-box/common/uot"
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/logger"
 	M "github.com/sagernet/sing/common/metadata"
 	N "github.com/sagernet/sing/common/network"
@@ -22,11 +24,12 @@ func RegisterInbound(registry *inbound.Registry) {
 
 type Inbound struct {
 	inbound.Adapter
-	router       adapter.ConnectionRouterEx
-	logger       logger.ContextLogger
-	listener     *listener.Listener
-	psk          [32]byte
-	replayFilter *ReplayFilter
+	router               adapter.ConnectionRouterEx
+	logger               logger.ContextLogger
+	listener             *listener.Listener
+	psk                  [32]byte
+	replayFilter         *ReplayFilter
+	heartbeatIntervalSec int
 }
 
 func NewInbound(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.AetherInboundOptions) (adapter.Inbound, error) {
@@ -36,12 +39,19 @@ func NewInbound(ctx context.Context, router adapter.Router, logger log.ContextLo
 		timeLimit = MaxTimeDiffMs
 	}
 
+	baseRouter := uot.NewRouter(router, logger)
+	muxRouter, err := mux.NewRouterWithOptions(baseRouter, logger, common.PtrValueOrDefault(options.Multiplex))
+	if err != nil {
+		return nil, err
+	}
+
 	in := &Inbound{
-		Adapter:      inbound.NewAdapter(C.TypeAether, tag),
-		router:       uot.NewRouter(router, logger),
-		logger:       logger,
-		psk:          psk,
-		replayFilter: NewReplayFilter(10000, timeLimit),
+		Adapter:              inbound.NewAdapter(C.TypeAether, tag),
+		router:               muxRouter,
+		logger:               logger,
+		psk:                  psk,
+		replayFilter:         NewReplayFilter(10000, timeLimit),
+		heartbeatIntervalSec: options.HeartbeatIntervalSec,
 	}
 
 	in.listener = listener.New(listener.Options{
@@ -67,7 +77,7 @@ func (in *Inbound) Close() error {
 }
 
 func (in *Inbound) NewConnection(ctx context.Context, conn net.Conn, metadata adapter.InboundContext, onClose N.CloseHandlerFunc) {
-	aetherConn, header, err := NewServerConn(conn, in.psk, in.replayFilter)
+	aetherConn, header, err := NewServerConn(conn, in.psk, in.replayFilter, in.heartbeatIntervalSec)
 	if err != nil {
 		N.CloseOnHandshakeFailure(conn, onClose, err)
 		return
